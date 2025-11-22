b9defabe50bd4fd07ad882d0e037e9edd7ab12b5
kaneko-mutsumi <oto.010wz@gmail.com>
2025-11-23 02:18:21 +0900

    初期設定

---
+++ src/test/java/com/example/volleyball_member_management/VolleyballMemberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.volleyball_member_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolleyballMemberManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
